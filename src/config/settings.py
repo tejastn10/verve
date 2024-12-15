4b9f509763fbb6974cddafcae322a626e20ef925
Tejas Nikhar <tejastn10@gmail.com>
2024-12-15 14:49:20 +0530

    feat: add settings file to load environment variables

---
+++ src/config/settings.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+# * Load environment variables from the .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), "../../.env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path=dotenv_path)
+
+
+class Settings:
+    OPEN_API_KEY = os.getenv("OPENAI_API_KEY")
+    TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+    TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+    TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+    TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
